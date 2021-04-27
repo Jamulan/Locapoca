797a91a0f66ee93c31424c20f86c9802499de067
jamulan <jamulan@ijuggle5.com>
2021-04-26 22:36:29 -0500

    Can now jump with a basic test enviroment

--- Assets/Player/Platformer.cs
+++ Assets/Player/Platformer.cs
@@ -5,6 +5,16 @@ using UnityEngine;
 public class Platformer : MonoBehaviour
 {
     public float speed;
+    public float jump_power;
+    public float set_jump_timer;
+
+    bool isGrounded = false;
+    public Collider2D ground_collider;
+    public LayerMask groundLayer;
+
+    private float jump_timer;
+    private float x;
+    private float time_since_grounded;
 
     Rigidbody2D rb;
 
@@ -12,18 +22,54 @@ public class Platformer : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        jump_timer = -1.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        x = Input.GetAxisRaw("Horizontal");
+        Jump();
+    }
+    void FixedUpdate()
+    {
         Move();
+        CheckIfGrounded();
     }
 
     void Move()
     {
-        float x = Input.GetAxisRaw("Horizontal");
         float moveBy = x * speed;
         rb.velocity = new Vector2(moveBy, rb.velocity.y);
+    }
+
+    void Jump()
+    {
+        if (isGrounded) 
+        {
+            jump_timer = 1.0f;
+        }
+        if (jump_timer > set_jump_timer)
+        {
+            float jump = Input.GetAxisRaw("Jump");
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y + (jump * jump_power * jump_timer * Time.deltaTime));
+            if(jump < 0.1)
+            {
+                jump_timer = -1.0f;
+            }
+            jump_timer -= Time.deltaTime;
+        }
+    }
+
+    void CheckIfGrounded()
+    {
+        isGrounded = ground_collider.IsTouchingLayers(groundLayer);
+        if(isGrounded)
+        {
+            time_since_grounded = 0.0f;
+        } else
+        {
+            time_since_grounded += Time.deltaTime;
+        }
     }
 }
